9e7df713d47acfa3637ebc2ed8167a37c9327bdc
Scob Beniamin <Beny@universityemanuel.com>
2017-06-01 19:23:36 +0300

    modificat din prmutari nercursiv in permutari recursiv

--- permutari.js
+++ permutari.js
@@ -1,51 +1,33 @@
-function permutari() 
+function permutari(k) 
 {
-  var k=0, s=[], valid=false, n=4; 
-  s[k] = 0;
-  
-  while (k >= 0) 
+  var solutii = [],i,n=4,
+  intrare = [1, 2, 3],
+  elemente = Array(intrare.length),
+  solutie = Array(intrare.length);
+  var i=0;
+  if (k == intrare.length)
   {
-    s[k]++;
-    valid = false;
-    while (!valid && s[k] <= n) 
-    {
-      if (ver(s, k + 1)) 
-      {
-        valid = true;
-      } 
-      else
-      {
-        s[k]++;
-      }
-    }
-    
-    if(valid)
-    {
-   if(k==n-1)
-   {
-   	console.log(s);
-   	}
-    else
-    {
-    k++;
-    s[k]=0;
-    }
-    }else{
-    k--;
-    }
+	 return solutii.push(solutie.slice());
   }
-}
-
-function ver(arr, k) 
-{
-  for (var i = 0; i < k - 1; i++) 
-  {
-    for (var j = i + 1; j < k; j++)
+	else
     {
-      if (arr[i] == arr[j])
-        return false
-    }
-  }
-  return true;
+        while (i < intrare.length) 
+        {
+           if (!elemente[i]) 
+        {
+             elemente[i] = true;
+             solutie[k] = intrare[i];
+             permutari(k + 1);
+             elemente[i] = false;
+             i++;
+        } 
+          else
+          {
+             i++;
+          }
+        }
+        return solutii.push(solutie.slice());  
+    }   
+	console.log(solutii[i]);
 }
 console.log(permutari(4));
